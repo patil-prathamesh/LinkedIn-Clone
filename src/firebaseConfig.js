870b9cc0371a26597e22a590aab2b1aed96f9b7e
patil-prathamesh <69043646+patil-prathamesh@users.noreply.github.com>
2024-04-22 08:42:39 +0530

    Update firebaseConfig.js

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -17,14 +17,14 @@ import { getStorage } from "firebase/storage";
 //     measurementId: "G-DL31W4KT1N"
 // };
 const firebaseConfig = {
-  apiKey: "AIzaSyAhV-vWz-8W4whhC0of4PscErfUysFYxuY",
-  authDomain: "linkedin-clone-173be.firebaseapp.com",
-  projectId: "linkedin-clone-173be",
-  storageBucket: "linkedin-clone-173be.appspot.com",
-  messagingSenderId: "957534038731",
-  appId: "1:957534038731:web:1c82c37a3f492bc09d894e",
-  measurementId: "G-DL31W4KT1N"
-};
+    apiKey: "AIzaSyATjaAVIczBmJVbGPoPH2yB-YB_a4oMZPs",
+    authDomain: "l-clone-e559d.firebaseapp.com",
+    projectId: "l-clone-e559d",
+    storageBucket: "l-clone-e559d.appspot.com",
+    messagingSenderId: "943612433961",
+    appId: "1:943612433961:web:bdb5bb44cb58c405cb6e38",
+    measurementId: "G-1DWFPVE4C6"
+  };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
