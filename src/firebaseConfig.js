50b8deab94b53ddee80ca2a44b4afb2f4180e591
patil-prathamesh <69043646+patil-prathamesh@users.noreply.github.com>
2024-04-22 08:24:10 +0530

    Update firebaseConfig.js

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -1,25 +1,21 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
-import { getFirestore } from "firebase/firestore";
-import { getStorage } from "firebase/storage";
+import { getAnalytics } from "firebase/analytics";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyAhV-vWz-8W4whhC0of4PscErfUysFYxuY",
-    authDomain: "linkedin-clone-173be.firebaseapp.com",
-    projectId: "linkedin-clone-173be",
-    storageBucket: "linkedin-clone-173be.appspot.com",
-    messagingSenderId: "957534038731",
-    appId: "1:957534038731:web:1c82c37a3f492bc09d894e",
-    measurementId: "G-DL31W4KT1N"
+  apiKey: "AIzaSyAhV-vWz-8W4whhC0of4PscErfUysFYxuY",
+  authDomain: "linkedin-clone-173be.firebaseapp.com",
+  projectId: "linkedin-clone-173be",
+  storageBucket: "linkedin-clone-173be.appspot.com",
+  messagingSenderId: "957534038731",
+  appId: "1:957534038731:web:1c82c37a3f492bc09d894e",
+  measurementId: "G-DL31W4KT1N"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);
-const firestore = getFirestore(app);
-const storage = getStorage(app);
-export { auth, app, firestore, storage };
+const analytics = getAnalytics(app);
